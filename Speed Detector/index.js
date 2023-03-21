019a1eaad7036b58387b8785dcae94ba5d09d189
Joan Kariuki <joan.kariuki01@student.moringaschool.com>
2023-03-21 04:53:09 +0300

    Added more styles

--- Speed Detector/index.js
+++ Speed Detector/index.js
@@ -7,20 +7,20 @@ function speedChecker(){
     //convert speed to merit points (5km/h = 1 point)
         let points = Math.floor((speed - 70) / 5)
     //message to display 
-        let message = "Demerit points: "
+        let message;
     //for points less than/equal to 0 i.e speed of 0-70 km/h, display "Ok"
         if(points <= 0) {
-            points = "Speed is ok"
+            message = "Speed is ok"
             }
     //for points between 1 and 12, display message + points
         else if(points >= 1 && points <=13) {
-            points = message + points
+            message = `You have ${points} demerit points`;
             }
     //for anything else, display "License suspended" and alert
         else  {
-            points = "License suspended"
+            message = "YOUR LICENSE IS SUSPENDED!"
             alert("License suspended!")
             }
     //display points on the  HTML element with id = "message"
-        document.getElementById("message").innerHTML = points;
+        document.getElementById("message").innerHTML = message;
     }
